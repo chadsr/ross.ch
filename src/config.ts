b5367440473c6ac0a8d2cd33c65bd009eb5ef25b
Ross Chadwick <ross@rchadwick.co.uk>
2019-01-07 01:20:12 +0100

    Contact form configuration

--- src/config.ts
+++ src/config.ts
@@ -6,12 +6,18 @@ export interface IConfig {
     port: number;
     debugLogging: boolean;
     sessionKey: string;
+    emailHost: string;
+    emailPassword: string;
+    emailAddress: string;
 }
 
 const config: IConfig = {
     port: +process.env.PORT || 3000,
     debugLogging: process.env.NODE_ENV == 'development',
     sessionKey: process.env.SESSION_KEY || 'some_secret_key',
+    emailHost: process.env.EMAIL_HOST || 'localhost',
+    emailPassword: process.env.EMAIL_PASSWORD || 'secret',
+    emailAddress: process.env.EMAIL_ADDRESS || 'foo@bar.com'
 };
 
 export { config };
