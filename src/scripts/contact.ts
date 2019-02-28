4de37e5f993f5f57ee331ca4fd4eb31a340853e6
Ross Chadwick <ross@rchadwick.co.uk>
2019-02-28 15:07:10 +0100

    Scroll error message into view instead of scrolling to top

--- src/scripts/contact.ts
+++ src/scripts/contact.ts
@@ -21,7 +21,6 @@ export default class ContactForm {
     }
 
     this._formSubmitBtn = <HTMLButtonElement>document.getElementById(formId + '-submit-btn');
-    this._formPage = <HTMLDivElement>this._form.parentElement.parentElement; // Store the outer page for use with scrolling
   }
 
   displayResponse(response: Response) {
@@ -42,6 +41,7 @@ export default class ContactForm {
         target.classList.add('success');
       } else {
         target.classList.add('error');
+        target.scrollIntoView();
       }
     });
   }
@@ -53,8 +53,6 @@ export default class ContactForm {
     if (response.success) {
       this.resetInput();
       this.resetLabels();
-    } else { // Otherwise scroll back to top so the user sees any errors
-      this._formPage.scrollTo(0, 0);
     }
 
     // Reset the submit button back to normal after 4s
