4726394e90eb67dad5b2e610199a339bbf02a040
Ross Chadwick <ross@rchadwick.co.uk>
2019-03-24 16:48:46 +0100

    No longer necessary with switch to caddy

--- renew_ssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run with root crontab to renew certificates like so:
-# renew_ssl.sh PATH_TO_COMPOSE_BIN PATH_TO_COMPOSE_YML
-
-$1 -f $2 run certbot renew --dry-run \
-&& $1 -f $2 kill -s SIGHUP webserver
