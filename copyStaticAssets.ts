0c52f0ca27b5c97c856a376566f4392d764a3eca
Ross Chadwick <git@ross.ch>
2019-07-29 23:31:42 +0200

    Moved resources to assets directory for better cleanliness

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -2,7 +2,6 @@ import * as shell from 'shelljs';
 
 shell.cp('-R', 'src/views/', 'dist/src/');
 shell.cp('-R', 'src/stylesheets/', 'dist/src/');
-shell.cp('-R', 'src/fonts/', 'dist/src/');
-shell.cp('-R', 'src/images/', 'dist/src/');
+shell.cp('-R', 'src/assets/', 'dist/src/');
 shell.cp('postcss.config.js', 'dist/');
 shell.cp('src/views/partials/webpack.hbs', 'dist/src/views/partials/webpack.hbs');
