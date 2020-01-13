e07e4de763b3153109ba861147fae98a8edec429
Ross Chadwick <git@ross.ch>
2020-01-13 20:27:12 +0100

    minor fixes

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -5,3 +5,4 @@ shell.cp( '-R', 'src/stylesheets/', 'dist/src/' );
 shell.cp( '-R', 'src/assets/', 'dist/src/' );
 shell.cp( 'postcss.config.js', 'dist/' );
 shell.cp( 'src/views/partials/webpack.hbs', 'dist/src/views/partials/webpack.hbs' );
+
