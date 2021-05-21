9ef278bbd8f8bcfa341fa09b4177451120abe19d
Ross <git@ross.ch>
2021-05-21 18:03:57 +0200

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+        "prettier"
     ]
 };
