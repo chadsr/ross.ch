e2517d1fddb0f74e441594a5df043b084780013e
Ross <git@ross.ch>
2021-12-06 13:28:17 +0100

    fixup contact form messages

--- src/errors.ts
+++ src/errors.ts
@@ -8,10 +8,10 @@ export interface ErrorMessagesI {
 
 const ErrorMessages: ErrorMessagesI = {
     InvalidEmail: 'Invalid Email',
-    InvalidNameShort: 'Got a less generic name?',
+    InvalidNameShort: 'Got a more unique name?',
     InvalidNameLong: 'Got a nickname?',
     InvalidMsg: 'Got something more to say?',
-    InvalidCaptcha: 'Captcha is invalid!',
+    InvalidCaptcha: 'Invalid Captcha',
 };
 
 export { ErrorMessages };
