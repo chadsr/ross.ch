ee966e4193df00b4671d0b1feef647071ace9e4b
Ross <git@ross.ch>
2024-06-27 22:29:16 +0200

    vitest fixes

--- functions/test/contact.spec.ts
+++ functions/test/contact.spec.ts
@@ -1,45 +1,42 @@
-import { createPagesEventContext } from 'cloudflare:test';
+import {
+    createPagesEventContext,
+    waitOnExecutionContext,
+} from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import { onRequestPost as contactOnRequestPost } from '../api/contact';
+import * as contactFunction from '../api/contact';
 
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe('Contact form worker', () => {
-    it('responds with 405 when not a POST', async () => {
-        const request = new IncomingRequest('http://example.com', {
-            method: 'PUT',
-        });
-        const ctx = createPagesEventContext({
-            request: request,
-            params: { path: '/api/contact' },
-        });
-        const response = await contactOnRequestPost(ctx);
-        expect(await response.status).toEqual(405);
-    });
-
     it('responds with 400 when no content-type set', async () => {
-        const request = new IncomingRequest('http://example.com', {
+        const request = new IncomingRequest('http://example.com/api/contact', {
             method: 'POST',
             headers: {},
         });
-        const ctx = createPagesEventContext({
+        const ctx = createPagesEventContext<
+            typeof contactFunction.onRequestPost
+        >({
             request: request,
             params: { path: '/api/contact' },
         });
-        const response = await contactOnRequestPost(ctx);
+        const response = await contactFunction.onRequestPost(ctx);
+        await waitOnExecutionContext(ctx);
         expect(await response.status).toEqual(400);
     });
 
     it('responds with 415 when wrong content-type set', async () => {
-        const request = new IncomingRequest('http://example.com', {
+        const request = new IncomingRequest('http://example.com/api/contact', {
             method: 'POST',
             headers: { 'content-type': 'application/json' },
         });
-        const ctx = createPagesEventContext({
+        const ctx = createPagesEventContext<
+            typeof contactFunction.onRequestPost
+        >({
             request: request,
             params: { path: '/api/contact' },
         });
-        const response = await contactOnRequestPost(ctx);
+        const response = await contactFunction.onRequestPost(ctx);
+        await waitOnExecutionContext(ctx);
         expect(await response.status).toEqual(415);
     });
 });
