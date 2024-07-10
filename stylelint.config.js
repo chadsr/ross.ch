5453222d26e8b788bff1ac7eb40fdf5573be3edc
Ross <git@ross.ch>
2024-07-10 14:01:28 +0200

    stylelint: fixup recommended

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,8 @@
 export default {
-    extends: ['stylelint-prettier/recommended', 'stylelint-scss'],
+    extends: [
+        'stylelint-prettier/recommended',
+        'stylelint-config-standard-scss',
+        'stylelint-scss',
+    ],
+    rules: { 'scss/operator-no-newline-after': null },
 };
