967eb3a71f3f18beba726f1957c011dbf6ae4de6
Ross <git@ross.ch>
2025-09-07 14:47:05 +0200

    wrangler: include worker dist

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ export default tseslint.config(
             'resources/**/*',
             'public/**/*',
             'static/**/*',
+            'dist/**/*',
             '.wrangler/**/*',
         ],
     },
