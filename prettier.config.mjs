fe66fcd922c4852e43849cd1920e44c5d898c13f
Ross <git@ross.ch>
2026-08-09 21:48:18 +0200

    prettier: enable prettier-plugin-go-template

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,23 @@
+import * as prettierPluginGoTemplate from '@htnabe/prettier-plugin-go-template';
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
 const config = {
     trailingComma: 'all',
     tabWidth: 4,
     semi: true,
     singleQuote: true,
+    plugins: [prettierPluginGoTemplate],
+    overrides: [
+        {
+            files: ['*.html'],
+            options: {
+                parser: 'go-template',
+            },
+        },
+    ],
 };
 
 export default config;
